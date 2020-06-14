20adb4b3e5e484f48f956b734c78b9425c0df2cd
egg man <hi@hi.com>
2020-06-13 23:37:22 -0400

    adding files

---
+++ powerblockCheck.py
@@ -0,0 +1,34 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+opts = Options()
+opts.headless = True
+
+driver = webdriver.Firefox(options=opts)  # Optional argument, if not specified will search path.
+driver.get('https://powerblock.com/product/sport/');
+
+try:
+  time.sleep(5)
+
+  type_element = driver.find_element_by_id('pa_dumbbell-type')
+  all_options = type_element.find_elements_by_tag_name('option');
+  for option in all_options:
+    if option.text == "Expandable":
+      option.click()
+      break
+
+  weight_element = driver.find_element_by_id('pa_weight')
+  for option in weight_element.find_elements_by_tag_name('option'):
+    if option.text == "5-90":
+      option.click()
+      break
+
+  #time.sleep(5)
+  in_stock_element = driver.find_element_by_class_name('out-of-stock')
+except Exception as e:
+  print("item is stocked???")
+  print(e)
+
+print("out of stock")
+driver.quit()
